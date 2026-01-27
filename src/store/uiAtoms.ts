7029489b39b91a88e27658ef13a6b22580004fcb
ecyrbe <ecyrbe@gmail.com>
2026-01-27 20:01:55 +0100

    feat: update zoom functionality to allow a maximum zoom level of 3 and adjust related styles

--- src/store/uiAtoms.ts
+++ src/store/uiAtoms.ts
@@ -4,11 +4,11 @@ export const zoomLevelAtom = atom<number>(1);
 export const printBlankValuesAtom = atom<boolean>(false);
 
 export const zoomInAtom = atom(null, (get, set) => {
-  set(zoomLevelAtom, Math.min(get(zoomLevelAtom) + 0.1, 2));
+  set(zoomLevelAtom, Math.min(get(zoomLevelAtom) + 0.1, 3));
 });
 
 export const zoomOutAtom = atom(null, (get, set) => {
-  set(zoomLevelAtom, Math.max(get(zoomLevelAtom) - 0.1, 0.5));
+  set(zoomLevelAtom, Math.max(get(zoomLevelAtom) - 0.1, 0.2));
 });
 
 export const resetZoomAtom = atom(null, (_get, set) => {
@@ -20,7 +20,7 @@ export const fitWidthAtom = atom(null, (_get, set) => {
   const page = document.querySelector(".page");
   if (page) {
     const scale = (window.innerWidth - 40) / (page as HTMLElement).offsetWidth;
-    set(zoomLevelAtom, Math.min(scale, 1.5));
+    set(zoomLevelAtom, Math.min(scale, 3));
   }
 });
 
@@ -29,6 +29,6 @@ export const fitHeightAtom = atom(null, (_get, set) => {
   if (page) {
     const scale =
       (window.innerHeight - 80) / (page as HTMLElement).offsetHeight;
-    set(zoomLevelAtom, Math.min(scale, 1.5));
+    set(zoomLevelAtom, Math.min(scale, 3));
   }
 });
